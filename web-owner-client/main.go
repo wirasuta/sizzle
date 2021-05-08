fbf79453cadda48ee27d1a3c8a04301856e9abd3
wirasuta <wirasutat@gmail.com>
2021-05-08 22:15:44 +0700

    Add rekey and revoke

--- web-owner-client/main.go
+++ web-owner-client/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
@@ -10,9 +9,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/spacemonkeygo/openssl"
 	cli "github.com/urfave/cli/v2"
@@ -76,21 +73,7 @@ func publishCertificate(cert *openssl.Certificate, certPrivateKey *openssl.Priva
 		log.Fatal("Fail to get domain from certificate common name")
 	}
 
-	var privateKeyStr string
-	if len(config.EthPrivateKey) > 0 {
-		privateKeyStr = config.EthPrivateKey
-	} else {
-		temp, err := ioutil.ReadFile(config.EthPrivateKeyFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		privateKeyStr = string(temp)
-	}
-	privateKey, err := crypto.HexToECDSA(privateKeyStr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	privateKey := utils.LoadPrivateKey(config)
 	sizzleAddress := common.HexToAddress(config.SizzleAddress)
 	client, err := ethclient.Dial(config.EthClientUrl)
 	if err != nil {
@@ -100,20 +83,7 @@ func publishCertificate(cert *openssl.Certificate, certPrivateKey *openssl.Priva
 	if err != nil {
 		log.Fatal(err)
 	}
-	fromAddress := crypto.PubkeyToAddress(privateKey.PublicKey)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	auth := bind.NewKeyedTransactor(privateKey)
-	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)
-	auth.GasLimit = uint64(300000)
-	auth.GasPrice = gasPrice
+	auth := utils.GenerateAuthBind(privateKey, client, 300000)
 
 	_, err = szlTxr.CertPublishRequest(auth, domain, string(pubKeyPem))
 	if err != nil {
@@ -127,6 +97,71 @@ func publishCertificate(cert *openssl.Certificate, certPrivateKey *openssl.Priva
 
 	log.Printf("Please add the following entry to %s DNS TXT record: \"sig=%s\"\n", domain, hex.EncodeToString(signedSzlAddress))
 
+	return nil
+}
+
+func rekeyCertificate(cert *openssl.Certificate, certPrivateKey *openssl.PrivateKey) error {
+	pubKey, err := cert.PublicKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pubKeyPem, err := pubKey.MarshalPKIXPublicKeyPEM()
+	if err != nil {
+		log.Fatal(err)
+	}
+	subject, err := cert.GetSubjectName()
+	if err != nil {
+		log.Fatal(err)
+	}
+	domain, ok := subject.GetEntry(openssl.NID_commonName)
+	if !ok {
+		log.Fatal("Fail to get domain from certificate common name")
+	}
+
+	privateKey := utils.LoadPrivateKey(config)
+	sizzleAddress := common.HexToAddress(config.SizzleAddress)
+	client, err := ethclient.Dial(config.EthClientUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	szlTxr, err := sizzle.NewSizzleTransactor(sizzleAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := utils.GenerateAuthBind(privateKey, client, 100000)
+
+	_, err = szlTxr.CertRekey(auth, domain, string(pubKeyPem))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedSzlAddress, err := (*certPrivateKey).SignPKCS1v15(openssl.SHA256_Method, []byte(config.SizzleAddress))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Please add the following entry to %s DNS TXT record: \"sig=%s\"\n", domain, hex.EncodeToString(signedSzlAddress))
+
+	return nil
+}
+
+func revokeCertificate(domain string) error {
+	privateKey := utils.LoadPrivateKey(config)
+	sizzleAddress := common.HexToAddress(config.SizzleAddress)
+	client, err := ethclient.Dial(config.EthClientUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	szlTxr, err := sizzle.NewSizzleTransactor(sizzleAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := utils.GenerateAuthBind(privateKey, client, 60000)
+
+	_, err = szlTxr.CertRevoke(auth, domain)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return nil
 }
 
@@ -156,6 +191,34 @@ func handlePublish(ctx *cli.Context) error {
 	publishCertificate(cert, &privateKey)
 
 	return nil
+}
+
+func handleRekey(ctx *cli.Context) error {
+	certByte, err := ioutil.ReadFile(ctx.Path("cert"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	privateKeyByte, err := ioutil.ReadFile(ctx.Path("privatekey"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	cert, err := openssl.LoadCertificateFromPEM(certByte)
+	if err != nil {
+		log.Println("here")
+		log.Fatal(err)
+	}
+	privateKey, err := openssl.LoadPrivateKeyFromPEM(privateKeyByte)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rekeyCertificate(cert, &privateKey)
+
+	return nil
+}
+
+func handleRevoke(ctx *cli.Context) error {
+	return revokeCertificate(ctx.String("domain"))
 }
 
 func handleInit(ctx *cli.Context) error {
@@ -205,6 +268,32 @@ func main() {
 					},
 				},
 				Action: handlePublish,
+			},
+			{
+				Name:  "rekey",
+				Usage: "Rekey published TLS certificate",
+				Flags: []cli.Flag{
+					&cli.PathFlag{
+						Name:    "privatekey",
+						Aliases: []string{"k"},
+					},
+					&cli.PathFlag{
+						Name:    "cert",
+						Aliases: []string{"c"},
+					},
+				},
+				Action: handleRekey,
+			},
+			{
+				Name:  "revoke",
+				Usage: "Revoke published TLS certificate",
+				Flags: []cli.Flag{
+					&cli.PathFlag{
+						Name:    "domain",
+						Aliases: []string{"d"},
+					},
+				},
+				Action: handleRevoke,
 			},
 			{
 				Name:  "init",
